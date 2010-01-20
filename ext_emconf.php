452118a784f89b0adcb5b81a756474a7013da5f6
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-20 13:37:37 +0000

    Released to TER as version 1.1.0 Updated documentation with new expression key "plugin"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/expressions/trunk@29102 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "expressions"
 #
-# Auto generated 04-10-2009 21:48
+# Auto generated 20-01-2010 14:36
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'context' => '5.0.0-0.0.0',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"a1ba";s:10:"README.txt";s:4:"ee2d";s:31:"class.tx_expressions_parser.php";s:4:"ad07";s:16:"ext_autoload.php";s:4:"38a1";s:12:"ext_icon.gif";s:4:"160e";s:14:"doc/manual.sxw";s:4:"378e";s:52:"interfaces/interface.tx_expressions_keyprocessor.php";s:4:"0d0a";s:58:"interfaces/interface.tx_expressions_valuepostprocessor.php";s:4:"5342";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"3bf0";s:10:"README.txt";s:4:"ee2d";s:39:"class.tx_expressions_contextstorage.php";s:4:"67d8";s:31:"class.tx_expressions_parser.php";s:4:"e8be";s:16:"ext_autoload.php";s:4:"38a1";s:12:"ext_icon.gif";s:4:"160e";s:17:"ext_localconf.php";s:4:"6d4d";s:14:"doc/manual.sxw";s:4:"7a02";s:52:"interfaces/interface.tx_expressions_keyprocessor.php";s:4:"6cf4";s:58:"interfaces/interface.tx_expressions_valuepostprocessor.php";s:4:"d945";s:50:"samples/class.tx_expressions_functionProcessor.php";s:4:"c39a";}',
 	'suggests' => array(
 	),
 );
