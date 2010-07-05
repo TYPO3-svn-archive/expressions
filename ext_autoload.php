7695a231dd35acbb4cbef55e09bd9f7c5ceb54b6
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-05 14:57:58 +0000

    Cleaned up path inclusion
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/expressions/trunk@35366 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -4,9 +4,9 @@
  *
  * $Id: ext_autoload.php 219 2009-09-01 11:48:35Z fsuter $
  */
-$extensionClassesPath = t3lib_extMgm::extPath('expressions') . 'Classes/';
+$extensionPath = t3lib_extMgm::extPath('expressions');
 return array(
-	'tx_expressions_parser' => t3lib_extMgm::extPath('expressions', 'class.tx_expressions_parser.php'),
-	'tx_expressions_viewhelpers_evaluateviewhelper' => $extensionClassesPath . 'ViewHelpers/EvaluateViewHelper.php',
+	'tx_expressions_parser' => $extensionClassesPath . 'class.tx_expressions_parser.php',
+	'tx_expressions_viewhelpers_evaluateviewhelper' => $extensionClassesPath . 'Classes/ViewHelpers/EvaluateViewHelper.php',
 );
 ?>
