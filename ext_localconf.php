4182e413ba6fb0f6e74db5e169663056c363728d
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-04 20:21:06 +0000

    Removed unnecessary usage of "context" extension hook
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/expressions/trunk@37685 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-
-	// Register the class for using the contextStorage hook
-$TYPO3_CONF_VARS['EXTCONF']['context']['contextStorage'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/class.tx_expressions_contextstorage.php:&tx_expressions_ContextStorage';
-?>
