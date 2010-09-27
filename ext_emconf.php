1775640bac0c0977f725963335bd3684bb2b3650
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-27 13:11:47 +0000

    Removed suggestion of extension "context" Released to TER as version 1.2.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/expressions/trunk@38650 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "expressions".
 #
-# Auto generated 25-08-2010 11:26
+# Auto generated 27-09-2010 15:10
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.2.2',
+	'version' => '1.2.3',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -37,10 +37,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'context' => '5.0.0-0.0.0',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"5a76";s:10:"README.txt";s:4:"ee2d";s:39:"class.tx_expressions_contextstorage.php";s:4:"67d8";s:31:"class.tx_expressions_parser.php";s:4:"6b1c";s:16:"ext_autoload.php";s:4:"fec6";s:12:"ext_icon.gif";s:4:"160e";s:17:"ext_localconf.php";s:4:"6d4d";s:42:"Classes/ViewHelpers/EvaluateViewHelper.php";s:4:"baa0";s:14:"doc/manual.pdf";s:4:"ce81";s:14:"doc/manual.sxw";s:4:"3770";s:52:"interfaces/interface.tx_expressions_keyprocessor.php";s:4:"6cf4";s:58:"interfaces/interface.tx_expressions_valuepostprocessor.php";s:4:"d945";s:50:"samples/class.tx_expressions_functionProcessor.php";s:4:"c39a";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"360f";s:10:"README.txt";s:4:"ee2d";s:31:"class.tx_expressions_parser.php";s:4:"6b1c";s:16:"ext_autoload.php";s:4:"fec6";s:12:"ext_icon.gif";s:4:"160e";s:42:"Classes/ViewHelpers/EvaluateViewHelper.php";s:4:"baa0";s:14:"doc/manual.pdf";s:4:"ce81";s:14:"doc/manual.sxw";s:4:"3770";s:52:"interfaces/interface.tx_expressions_keyprocessor.php";s:4:"6cf4";s:58:"interfaces/interface.tx_expressions_valuepostprocessor.php";s:4:"d945";s:50:"samples/class.tx_expressions_functionProcessor.php";s:4:"c39a";}',
 	'suggests' => array(
 	),
 );
