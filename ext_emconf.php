2121024c80a90346e2ccc292cf2ec474f8658477
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-04 10:23:59 +0000

    Released to TER as version 1.4.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/expressions/trunk@46186 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "expressions".
 #
-# Auto generated 28-12-2010 16:03
+# Auto generated 04-04-2011 12:23
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.3.0',
+	'version' => '1.4.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"dbe8";s:10:"README.txt";s:4:"ee2d";s:31:"class.tx_expressions_parser.php";s:4:"0b3f";s:16:"ext_autoload.php";s:4:"fec6";s:12:"ext_icon.gif";s:4:"160e";s:42:"Classes/ViewHelpers/EvaluateViewHelper.php";s:4:"baa0";s:14:"doc/manual.pdf";s:4:"ebf1";s:14:"doc/manual.sxw";s:4:"f14a";s:52:"interfaces/interface.tx_expressions_keyprocessor.php";s:4:"6cf4";s:58:"interfaces/interface.tx_expressions_valuepostprocessor.php";s:4:"d945";s:50:"samples/class.tx_expressions_functionProcessor.php";s:4:"c39a";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"717b";s:10:"README.txt";s:4:"ee2d";s:31:"class.tx_expressions_parser.php";s:4:"0b3f";s:16:"ext_autoload.php";s:4:"fec6";s:12:"ext_icon.gif";s:4:"160e";s:42:"Classes/ViewHelpers/EvaluateViewHelper.php";s:4:"42b4";s:14:"doc/manual.pdf";s:4:"daf5";s:14:"doc/manual.sxw";s:4:"95bd";s:52:"interfaces/interface.tx_expressions_keyprocessor.php";s:4:"6cf4";s:58:"interfaces/interface.tx_expressions_valuepostprocessor.php";s:4:"d945";s:50:"samples/class.tx_expressions_functionProcessor.php";s:4:"c39a";}',
 	'suggests' => array(
 	),
 );
