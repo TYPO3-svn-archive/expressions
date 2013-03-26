8d0157b5bad3f8f9db9d50e4df10cf1c4ff85fd9
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-26 21:30:08 +0000

    Released to TER as version 1.6.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/expressions/trunk@73779 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "expressions".
-#
-# Auto generated 10-07-2012 10:12
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "expressions".
+ *
+ * Auto generated 26-03-2013 22:29
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Generic expression parser',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.5.0',
+	'version' => '1.6.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"d490";s:31:"class.tx_expressions_parser.php";s:4:"cab0";s:16:"ext_autoload.php";s:4:"20d3";s:12:"ext_icon.gif";s:4:"160e";s:10:"README.txt";s:4:"ee2d";s:42:"Classes/ViewHelpers/EvaluateViewHelper.php";s:4:"8e4a";s:14:"doc/manual.pdf";s:4:"cff8";s:14:"doc/manual.sxw";s:4:"0df4";s:52:"interfaces/interface.tx_expressions_keyprocessor.php";s:4:"6cf4";s:58:"interfaces/interface.tx_expressions_valuepostprocessor.php";s:4:"d945";s:50:"samples/class.tx_expressions_functionProcessor.php";s:4:"c39a";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"c6b6";s:31:"class.tx_expressions_parser.php";s:4:"d32e";s:16:"ext_autoload.php";s:4:"20d3";s:12:"ext_icon.gif";s:4:"160e";s:10:"README.txt";s:4:"ee2d";s:42:"Classes/ViewHelpers/EvaluateViewHelper.php";s:4:"8e4a";s:14:"doc/manual.pdf";s:4:"287c";s:14:"doc/manual.sxw";s:4:"08f1";s:52:"interfaces/interface.tx_expressions_keyprocessor.php";s:4:"6cf4";s:58:"interfaces/interface.tx_expressions_valuepostprocessor.php";s:4:"d945";s:50:"samples/class.tx_expressions_functionProcessor.php";s:4:"c39a";}',
 	'suggests' => array(
 	),
 );
